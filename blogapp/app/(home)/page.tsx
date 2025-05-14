26087b60dd3a51abc86f2a127f58a481f8428e48
Dewan Shakib <shakib.devv@gmail.com>
2025-05-15 05:39:58 +0600

    moving for windows install

---
+++ blogapp/app/(home)/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/home/header/navbar";
+import HeroSection from "@/components/home/hero/hero-section";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <HeroSection/>
+    </div>
+  );
+}
---
+++ blogapp/components/home/hero/hero-section.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section className='relative min-h-[600px] w-full overflow-hidden bg-gradient-to-r from-purple-500 via-indigo-700 to-indigo-950'>
+        {/* Gradient Overlay */}
+
+    </section>
+  )
+}
+
+export default HeroSection
